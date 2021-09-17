64c20f544b5973d2cc4885111455abb866bd33cf
na0495 <saadmrabet008@gmail.com>
2021-09-17 09:02:23 +0100

    add swager

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,24 @@
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API of Geo-plot",
+      default_version='v1',
+      description="Api's endpoint of geo-plot",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
