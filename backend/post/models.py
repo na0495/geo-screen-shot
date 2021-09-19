295b8997a7628787f78c9adcb9c86936ee318186
na0495 <saadmrabet008@gmail.com>
2021-09-19 13:12:38 +0100

    update plot model

--- backend/post/models.py
+++ backend/post/models.py
@@ -10,7 +10,6 @@ def upload_to(instance, filename):
 # Plot model
 class Plot(models.Model):
     name = models.CharField(max_length=200)
-    size = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(_("Image"), upload_to=upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
 
