04c9627a15d25d4d136ff5d0082556389ff54011
na0495 <saadmrabet008@gmail.com>
2021-09-19 13:27:17 +0100

    config plot list

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Plot } from './models/plot.model';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  plotList: Plot [] = [];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private _postService: PostService
+  ) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit() {
+    this._postService.getAll().subscribe(
+      data => this.plotList = data,
+      error => console.log(error)
+    );
+  }
 }
