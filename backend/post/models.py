71fefb232561ca233a7fc7ed35329da31adfa3a9
na0495 <saadmrabet008@gmail.com>
2021-09-21 12:31:13 +0100

    create new model

--- backend/post/models.py
+++ backend/post/models.py
@@ -11,7 +11,17 @@ def upload_to(instance, filename):
 class Plot(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(_("Image"), upload_to=upload_to)
+    coordinates = models.ForeignKey('Coordinates', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Coordinates(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.latitude) + ' ' + str(self.longitude)
